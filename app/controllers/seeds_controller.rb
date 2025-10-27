9419731801bb83975e45648294538bb875779e2c
lucasleandro1 <lukkassoliveira2215@gmail.com>
2025-10-26 23:14:10 -0300

    Fix setup_images

--- app/controllers/seeds_controller.rb
+++ app/controllers/seeds_controller.rb
@@ -1,4 +1,13 @@
 class SeedsController < ApplicationController
+  def setup_images
+    system("bin/rails importmap:install")
+    system("bin/rails importmap:pin stimulus")
+    system("bin/rails importmap:pin_all_from app/javascript/controllers")
+    system("bundle exec rails assets:precompile")
+    system("bundle exec rails importmap:install")
+    render plain: "Comandos executados!"
+  end
+
   def run
     # só execute uma vez ou com algum token
     system("rails db:drop db:create db:migrate")
