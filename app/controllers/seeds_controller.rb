005e3de49ef1b73fc042376cec4a372dea402958
lucasleandro1 <lukkassoliveira2215@gmail.com>
2025-10-26 23:03:46 -0300

    Improve run seed

--- app/controllers/seeds_controller.rb
+++ app/controllers/seeds_controller.rb
@@ -1,6 +1,7 @@
 class SeedsController < ApplicationController
   def run
     # só execute uma vez ou com algum token
+    system("rails db:drop db:create db:migrate")
     load Rails.root.join("db/seeds.rb")
     render plain: "Seed executado!"
   end
